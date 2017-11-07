2cf257a20623235363415d27bf1ae3c44dcfc59e
Hugo <hugo.vdh@hotmail.com>
2017-11-07 17:24:50 +0100

    Première version de DbContract

--- app/src/main/java/com/example/hugo/coursintent/DbContract.java
+++ app/src/main/java/com/example/hugo/coursintent/DbContract.java
@@ -1,9 +1,19 @@
 package com.example.hugo.coursintent;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by Hugo on 07/11/2017.
  */
 
 public class DbContract {
+    private DbContract(){
 
+    }
+
+    public static final class StudentEntries implements BaseColumns {
+        public static final String TABLE_NAME = "student";
+        public static final String FIRST_NAME = "firstname";
+        public static final String LAST_NAME = "lastname";
+    }
 }
